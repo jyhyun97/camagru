05594484e8204938f1a6e2b21d85ff552c3c17f0
EC2 Default User <ec2-user@ip-172-31-45-226.ap-northeast-2.compute.internal>
2023-04-21 08:03:28 +0000

    [feat] 메인페이지 레이아웃 구성

---
+++ main.php
@@ -0,0 +1,23 @@
+<!-- 필요한 정보
+로그인 여부
+총 이미지 수, 한 번에 로드되는 이미지 수, 현재 로드된 이미지 수, 현재 페이지 번호
+-->
+
+<div>
+    <header class="navi">
+        <button>홈</button>
+        <button>로그인</button>
+        <button>회원가입</button>
+    </header>
+    <div class="content">
+        <div class="gallary">
+            <img src="https://picsum.photos/300/200">
+            <img src="https://picsum.photos/300/200">
+        </div>
+        <div class="buttons">
+            <button><</button>
+            <button>></button>
+            <button>업로드</button>
+        </div>
+    </div>
+</div>
