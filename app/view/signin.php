fec011e81cfc36e64733dab3dfb4af6b94c4c1b8
jeonhyun <jeonhyun@student.42seoul.kr>
2023-05-02 04:41:31 +0000

    [feat] 로그인 회원가입 폼 추가

--- app/view/signin.php
+++ app/view/signin.php
@@ -1,4 +1,16 @@
 <div>
-    로그인
-    <input></input>
+    <h2>로그인</h2>
+    <form class="signform">
+        <div class="signform-line">
+            <label>이메일</label>
+            <input type="email" id="signin-email">
+        </div>
+        <div class="signform-line">
+            <label>비밀번호</label>
+            <input type="password" id="signin-password">
+        </div>
+        <button>로그인</button><!-- 폼 내용 취합해 객체로 만들어서 요청 보내기 이 부분은 컨트롤러 사용 -->
+        <button>취소</button><!-- 클릭 시 모달 닫기-->
+    </form>
+    <!-- 추후 유효성검사메시지 추가 -->
 </div>
