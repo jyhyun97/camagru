96c4b79dd56d99cf573379ed031e2c0d7dd04668
jeonhyun <jeonhyun@student.42seoul.kr>
2023-05-09 05:21:57 +0000

    [refactor] 모달 activeModal, inactiveModal로 기능 분리

--- app/view/modal.js
+++ app/view/modal.js
@@ -1,14 +1,26 @@
 export function activeModal(modalName) {
     const modalFrame = document.getElementById('modal');
     const modal = document.getElementById(modalName);
-    if (modalFrame.style.visibility == 'visible')
-    {
-        modalFrame.style.visibility = 'hidden';
-        modal.style.display = 'none';
-    }
-    else
-    {
-        modalFrame.style.visibility = 'visible';
-        modal.style.display = 'block';
-    }
+    
+    modalFrame.style.visibility = 'visible';
+    modal.style.display = 'block';
+    
 }
+export function inactiveModal() {
+    const modalFrame = document.getElementById('modal');
+    const signform = document.getElementsByClassName('signform');
+
+    modalFrame.style.visibility = 'hidden';
+    Array.from(signform).forEach(ele => {
+        ele.style.display = 'none';
+    });
+}
+
+const modalBackground = document.getElementById('modal-background');
+const modalCancel = document.getElementsByClassName('modal-cancel');
+
+modalBackground.addEventListener('click', () => inactiveModal());
+Array.from(modalCancel).forEach(ele => {
+    const id = ele.id.replace('cancel', 'form');
+    ele.addEventListener('click', () => inactiveModal());
+});
--- app/view/modal.php
+++ app/view/modal.php
@@ -7,17 +7,4 @@
         ?>
     </div>
 </div>
-<script type="module">
-    import { activeModal } from './app/view/modal.js';
-    const modalBackground = document.getElementById('modal-background');
-    modalBackground.addEventListener('click', () => activeModal());
-
-    const signinForm = document.getElementById('signin-form');
-    const signupForm = document.getElementById('signup-form');
-    const modalCancel = document.getElementsByClassName('modal-cancel');
-
-    Array.from(modalCancel).forEach(ele => {
-        const id = ele.id.replace('cancel', 'form');
-        ele.addEventListener('click', () => activeModal(id));
-    });
-</script>
+<script src="app/view/modal.js" type="module"></script>
