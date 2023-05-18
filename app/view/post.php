9f83f35b36dd00822ef981284a368812dd1b4e2f
jeonhyun <jeonhyun@student.42seoul.kr>
2023-05-18 09:35:23 +0000

    [fix] 라우터 개선

--- app/view/post.php
+++ app/view/post.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Camagru</title>
-    <link rel="stylesheet" type="text/css" href="../styles/common.css">
-    <link rel="stylesheet" type="text/css" href="../styles/post.css">
+    <link rel="stylesheet" type="text/css" href="/app/styles/common.css">
+    <link rel="stylesheet" type="text/css" href="/app/styles/post.css">
 </head>
 
 <body>
@@ -16,6 +16,11 @@
     ?>
     <div class="content">
         <img src="https://picsum.photos/600/300">
+        <?php
+        $currentUrl = $_SERVER['REQUEST_URI'];
+        echo $currentUrl;
+        // echo "<img src='imageId'>";
+        ?>
         <label>좋아요 개수</label>
         <button>좋아요</button>
         <div>
@@ -30,10 +35,6 @@
     </div>
 </body>
 
-<?php
-$currentUrl = $_SERVER['REQUEST_URI'];
-print_r($currentUrl);
-?>
 
 <!-- 
 기본적으로 /post/postId로 갑시다...
