078788a1139e2ec6efdf8f4358cacf91abd07316
jeonhyun <jeonhyun@student.42seoul.kr>
2023-06-06 09:02:05 +0000

    [etc] validateEmail 테스트 추가

--- app/test/signupTest.php
+++ app/test/signupTest.php
@@ -8,7 +8,19 @@ class SignupTest extends Test
             $this->assertEquals(TestController::postSignupProcess(
                 'abcd@1234', 'bb', 'abcd1234'), '중복된 닉네임입니다.');
             //테스트 작성
-            echo "testPlus success!\n";
+            print_r("testPlus success!\n");
+        } catch (Exception $e) {
+            echo "$e\n";
+        }
+    }
+    public function testValidate()
+    {
+        try {
+            $this->assertTrue(TestController::validateEmail(
+                'aa@bb.com'));
+            $this->assertFalse(TestController::validateEmail(
+                'abcd.com'));
+            print_r("testValidate success!\n");
         } catch (Exception $e) {
             echo "$e\n";
         }
@@ -16,7 +28,12 @@ class SignupTest extends Test
 
     public function run()
     {
+        $startTime = microtime(true);
         $this->testDup();
+        $this->testValidate();
+        $endTime = microtime(true);
+        $time = number_format($endTime - $startTime, 6);
+        echo $time."초 소요됨\n";
     }
 }
 
