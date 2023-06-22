3cf8ddcb24dea749647240ad6c13b2a25391c30c
jeonhyun <jeonhyun@student.42seoul.kr>
2023-06-22 08:57:25 +0000

    [fix] 생각해보니 파일 합칠 필요까지는 없었음

--- app/view/modal/modal.php
+++ app/view/modal/modal.php
@@ -1,40 +1,10 @@
 <div id="modal">
     <div id="modal-background"></div>
     <div id="modal-content">
-        <div class="signform" id="signin-form">
-            <h2>로그인</h2>
-            <div class="signform-line">
-                <label>이메일</label>
-                <input type="email" id="signin-email">
-            </div>
-            <div class="signform-line">
-                <label>비밀번호</label>
-                <input type="password" id="signin-password">
-            </div>
-            <div class="signform-button">
-                <button id="signin-submit" class="btn btn-primary">로그인</button>
-                <button class="modal-cancel btn btn-default" id="signin-cancel">취소</button>
-            </div>
-        </div>
-        <div class="signform" id="signup-form">
-            <h2>회원가입</h2>
-            <div class="signform-line">
-                <label>이메일</label>
-                <input type="email" id="signup-email" name="email">
-            </div>
-            <div class="signform-line">
-                <label>닉네임</label>
-                <input type="text" id="signup-username" name="username">
-            </div>
-            <div class="signform-line">
-                <label>비밀번호</label>
-                <input type="password" id="signup-password" name="password">
-            </div>
-            <div class="signform-button">
-                <button id="signup-submit" class="btn btn-primary">회원가입</button>
-                <button class="modal-cancel btn btn-default" id="signup-cancel">취소</button>
-            </div>
-        </div>
+        <?php
+        include_once 'app/view/modal/signup.php';
+        include_once 'app/view/modal/signin.php';
+        ?>
     </div>
 </div>
 <script src="/app/view/modal/modal.js" type="module"></script>
---
+++ app/view/modal/signup.php
@@ -0,0 +1,19 @@
+<div class="signform" id="signup-form">
+    <h2>회원가입</h2>
+    <div class="signform-line">
+        <label>이메일</label>
+        <input type="email" id="signup-email" name="email">
+    </div>
+    <div class="signform-line">
+        <label>닉네임</label>
+        <input type="text" id="signup-username" name="username">
+    </div>
+    <div class="signform-line">
+        <label>비밀번호</label>
+        <input type="password" id="signup-password" name="password">
+    </div>
+    <div class="signform-button">
+        <button id="signup-submit" class="btn btn-primary">회원가입</button>
+        <button class="modal-cancel btn btn-default" id="signup-cancel">취소</button>
+    </div>
+</div>
