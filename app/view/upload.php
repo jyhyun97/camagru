8b5c98008a267279f7a9f906e52bec6b032e1713
jeonhyun <jeonhyun@student.42seoul.kr>
2023-06-26 08:27:24 +0000

    [refactor] view에서 ArrayObject 및 iterator 적용

--- app/view/upload.php
+++ app/view/upload.php
@@ -20,7 +20,7 @@
         <div class="main-content">
             <div id="upload-left">
                 <div id="output">
-                    <canvas id='sticky-canvas'></canvas>
+                    <canvas id='sticky-canvas' width="640" height="480"></canvas>
                     <video id="video"></video>
                     <canvas id="canvas" width="640" height="480" hidden></canvas>
                     <img id="photo" hidden />
@@ -32,14 +32,12 @@
                 <div id="sticky-list">
                     스티커 목록
                     <?php
-                        $dirpath = 'img/sticky/';
-                        $dir = scandir($dirpath);
-                        
-                        foreach ($dir as $key) {
-                            if ($key === '.' || $key === '..')
+                        foreach (new DirectoryIterator('img/sticky/') as $fileInfo)
+                        {
+                            $filePath = $fileInfo->getPathname();
+                            if ($fileInfo->getFileName() === '.' || $fileInfo->getFileName() === '..')
                                 continue;
-                            $stickyPath = $dirpath.$key;
-                            echo "<img src='$stickyPath' class='sticky-image' id='$stickyPath' onclick='selectSticky(event)'>";//선택 가능할 것..
+                            echo "<img src='$filePath' class='sticky-image' id='$filePath' onclick='selectSticky(event)'>";
                         }
                     ?>
                 </div>
@@ -49,15 +47,20 @@
                     생성한 이미지 목록
                     <?php
                     $username = $_SESSION['username'];
-                    $images = Maincontroller::getImagesByUsername($username);
+                    $imagesObj = new ArrayObject(Maincontroller::getImagesByUsername($username));
+                    $imagesIt = $imagesObj->getIterator();
 
-                    foreach ($images as $ele) {
-                        $src = $ele['image'];
-                        $imageId = $ele['imageId'];
+                    while ($imagesIt->valid())
+                    {
+                        $src = $imagesIt->current()['image'];
+                        $imageId = $imagesIt->current()['imageId'];
+
                         echo "<div class='capture'>";
                         echo "<img src=\"$src\" class='captured-image' onclick='selectImage(event)' id='captured-image-$imageId'>";
                         echo "<button onclick='deleteImage(event)' class='capture-delete-button' data-image-id='$imageId'>삭제</button>";
                         echo "</div>";
+                        
+                        $imagesIt->next();
                     }
                     ?>
                 </div>
