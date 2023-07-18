e780b5ba3fb3e64d1814a4f77d669b80bab236ad
jeonhyun <jeonhyun@student.42seoul.kr>
2023-07-18 17:02:39 +0900

    [fix] 404 update

--- app/view/404.php
+++ app/view/404.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-    </head>
-    <body>
-        <h1>404 Page</h1>
-        <div>유효하지 않은 접근입니다.</div>
-        <button onclick="location.href='/'">메인으로 이동</button>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camagru</title>
+    <link rel="shortcut icon" href="/app/asset/favicon.ico">
+</head>
+
+<body>
+    <h1>404 Page</h1>
+    <div>유효하지 않은 접근입니다.</div>
+    <button onclick="location.href='/'">메인으로 이동</button>
+</body>
+
 </html>
